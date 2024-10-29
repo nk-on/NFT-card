b3fef997c407e1b66829211ba06b9d98d32fdba6
nk-on <johndoe@example.com>
2024-10-29 23:15:20 +0400

    Adding and positioning equilibrium image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-
+import equilibrium  from './assets/image-equilibrium.jpg'
 function App() {
   return <>
    <Card></Card>
   </>;
 }
 function Card() {
-  return <div className="card"></div>;
+  return <div className="card">
+    <img className = 'equilibrium' src= {equilibrium} alt='equilibrium'/>
+  </div>;
 }
 export default App;
