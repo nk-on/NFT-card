5bbd9299ece8914ec030250b28a1b765208d489d
nk-on <johndoe@example.com>
2024-10-30 07:58:29 +0400

    Moving Card component to another file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import './App.css';
-import equilibrium  from './assets/image-equilibrium.jpg'
+import Card from './Components/Card/Card'
 function App() {
   return <>
    <Card></Card>
   </>;
-}
-function Card() {
-  return <div className="card">
-    <img className = 'equilibrium' src= {equilibrium} alt='equilibrium'/>
-  </div>;
 }
 export default App;
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,9 @@
+import  equilibrium  from '../../assets/image-equilibrium.jpg'
+import './Card.css'
+export default function Card() {
+  return (
+    <div className="card">
+      <img className="equilibrium" src={equilibrium} alt="equilibrium" />
+    </div>
+  );
+}
